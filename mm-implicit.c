440ad27f125c78321098afd6a6984db3f61d666f
Jun Jahyun <equusjun@nate.com>
2017-12-09 03:00:29 +0900

    mm-implicit.c 구현 중

--- mm-implicit.c
+++ mm-implicit.c
@@ -41,6 +41,27 @@
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(p) (((size_t)(p) + (ALIGNMENT-1)) & ~0x7)
 
+#define WSIZE 4
+#define DSIZE 8
+#define CHUNKSIZE (1<<12)
+#define OVERHEAD 8
+
+#define MAX(x,y) ((x) > (y) ? (x) : (y))
+
+//크기와 할당된 비트를 word로 묶음
+#define PACK(size, alloc) ((size) | (alloc))
+
+//주소 p에서 word 읽기, 쓰기
+#define GET(p) (*(size_t *)(p))
+#define PUT(p,val) (*(size_t *)(p) = (val))
+#define GET_SIZE(p) (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+#define FTRP(bp) ((char*)(bp) + GET_SIZE((char*)(bp) -WSIZE))
+#define PREV_BLKP(bp) ((char*)(bp) - GET_SIZE((char*)(bp) -DSIZE))
+
+
+
 /*
  * Initialize: return -1 on error, 0 on success.
  */
